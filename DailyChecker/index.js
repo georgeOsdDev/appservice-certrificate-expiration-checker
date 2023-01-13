5fba918dcb675a451e64ce7ef923f40e1b8beefa
Takeharu Oshida <toshida@microsoft.com>
2023-01-13 20:29:27 +0900

    init

---
+++ DailyChecker/index.js
@@ -0,0 +1,40 @@
+const { DefaultAzureCredential } = require("@azure/identity");
+const { ResourceGraphClient } = require("@azure/arm-resourcegraph");
+
+const credentials = new DefaultAzureCredential();
+const client = new ResourceGraphClient(credentials);
+
+const expireThreshold = process.env["expireThreshold"] || "30"; // days
+const query = `Resources
+| where subscriptionId == "${process.env["subscriptionId"]}"
+| where type == "microsoft.certificateregistration/certificateorders"
+| where properties["expirationTime"] < datetime_add("day", ${expireThreshold}, now())
+| project id, resourceGroup, name, expirationTime=tostring(properties["expirationTime"]), distinguishedName=properties["distinguishedName"], provisioningState=properties["provisioningState"], autoRenew=properties["autoRenew"]
+| order by expirationTime asc`;
+
+module.exports = async function (context, myTimer) {
+    context.log("Start to check App Service Certificates Expirination");
+    try {
+        const result = await client.resources({query},{ resultFormat: "table" });
+        if (result.totalRecords > 0) {
+
+            context.log.warn(`Found App Service Certificates Expiring in the next ${expireThreshold} days, Count: ${result.totalRecords}`);
+            // Track avobe warning log with Application Insights Log Alert
+            // https://learn.microsoft.com/en-us/azure/azure-monitor/alerts/alerts-types#log-alerts
+            //
+            // traces
+            // | where customDimensions.Category == "Function.DailyChecker.User"
+            // | where customDimensions.LogLevel == "Warning"
+            // | where message startswith "Found App Service Certificates Expiring in the next"
+
+            result.data.forEach((row) => {
+                context.log(`App Service Certificate is about to expire ${JSON.stringify(row)}`)
+            });
+        } else {
+            context.log(`No App Service Certificate Expiring in the next ${expireThreshold} days`);
+        }
+    } catch (error) {
+        context.log.error("Failed to execute query", error);
+    }
+    context.log("Finish to check App Service Certificates Expirination");
+};
---
+++ README.md
@@ -0,0 +1,21 @@
+# Azure App Service Certificate expiration checker
+
+Run [Azure Resource Graph](https://learn.microsoft.com/en-us/azure/governance/resource-graph/overview) API daily by using [Azure Functions TimerTrigger](https://learn.microsoft.com/en-us/azure/azure-functions/functions-bindings-timer?tabs=in-process&pivots=programming-language-csharp) to check [App Service Certificate](https://learn.microsoft.com/en-us/azure/app-service/configure-ssl-certificate?tabs=apex%2Cportal) status.
+If a certificate that is about to expire is found, alert notification will send using [Azure Monitor](https://learn.microsoft.com/en-us/azure/azure-monitor/alerts/alerts-types#log-alerts)
+
+## Usage
+
+1. Enable [Managed Identity](https://learn.microsoft.com/en-us/azure/app-service/overview-managed-identity?toc=%2Fazure%2Fazure-functions%2Ftoc.json&tabs=portal%2Chttp) for Function App
+
+2. Assign Subscription's built in [Reader](https://learn.microsoft.com/en-us/azure/role-based-access-control/built-in-roles#reader) Role to above id.
+
+    ![](docs/RoleAssignments.jpg)
+
+3. Edit AppSettings of Function App
+
+   - `subscriptionId` : target subscription to check
+   - `expireThreshold` : expire threshold days, default 30
+
+4. Setup Azure Monitor log alert
+
+    ![](docs/AlertRule.jpg)
